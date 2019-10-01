6ba1b79a99e532afc70f1e7b3a8e4354464a4397
peter.linlin <peter.linlin@gmail.com>
2019-10-01 03:35:32 -0700

    handle favicon

--- serviceway/serviceway/urls.py
+++ serviceway/serviceway/urls.py
@@ -6,10 +6,12 @@ from datetime import datetime
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
+from django.views.generic import RedirectView
 from app import forms, views
 
 urlpatterns = [
     path('', include(('app.urls', "app"), "appurls")),
+    path('favicon\.ico', RedirectView.as_view(url='/static/app/misc/favicon.ico', permanent=True)),
     path('contact', views.contact, name='contact'),
     path('about/', views.about, name='about'),
     path('seed/', views.seed, name='seed'),
